b6147b6de44b4a4194422784b3415083ec39689d
Eyad Hasan <ehasan3@gmu.edu>
2017-04-12 20:27:37 -0400

    Added realm to the pod file. Nothing written in relation to Realm yet, but this is the start.

---
+++ WhatsOpen/WhatsOpen/SRCTRealm.swift
@@ -0,0 +1,11 @@
+//
+//  SRCTRealm.swift
+//  WhatsOpen
+//
+//  Created by Eyad Hasan on 4/12/17.
+//  Copyright © 2017 Patrick Murray. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
