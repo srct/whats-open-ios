4efc05c2a3a7825420806520ec92670f59f4959a
Zach Knox <zmknox@gmail.com>
2017-07-29 18:02:31 -0400

    Cool about screen with contributors and version numbers

--- WhatsOpen/WhatsOpen/AboutScreenViewController.swift
+++ WhatsOpen/WhatsOpen/AboutScreenViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class AboutScreenViewController: UIViewController {
+class AboutScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+	@IBOutlet var versionLabel: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+		let build = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+		
+		let versionstring = "Version " + version + " (" + build + ")"
+		
+		versionLabel.text = versionstring
+		
+		
+		
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +31,35 @@ class AboutScreenViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+	
+	let contributors = [
+		"Eyad Hasan",
+		"Zach Knox",
+		"Patrick Murray",
+		"Jason Yeomans"
+	]
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return contributors.count
+	}
+	
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return "Thanks to the contributors to What's Open!"
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Contributor", for: indexPath)
+		
+		cell.textLabel!.text = contributors[indexPath.row]
+		
+		return cell
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	
 
     /*
     // MARK: - Navigation
